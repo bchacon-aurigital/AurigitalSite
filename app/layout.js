43b2b462dcaff37ea32647bc7067097ad33133ae
Marcela Cascante <marce@mainlydigitalworks.com>
2025-07-03 10:27:56 -0600

    AI and Meta Pixel

--- app/layout.js
+++ app/layout.js
@@ -1,141 +1,152 @@
 import "./globals.css";
-import { LoadingProvider } from './context/LoadingContext';
-import { ContactModalProvider } from './context/ContactModalContext';
-import { LanguageProvider } from './context/LanguageContext';
-import { ChatProvider } from './context/ChatContext';
-import LanguageSwitcher from './components/LanguageSwitcher';
-import ChatBot from './components/ChatBot';
-import Script from 'next/script';
+import { LoadingProvider } from "./context/LoadingContext";
+import { ContactModalProvider } from "./context/ContactModalContext";
+import { LanguageProvider } from "./context/LanguageContext";
+import { ChatProvider } from "./context/ChatContext";
+import LanguageSwitcher from "./components/LanguageSwitcher";
+import ChatBot from "./components/ChatBot";
+import Script from "next/script";
 
 export const metadata = {
-  metadataBase: new URL('https://www.aurigital.com/'),
+  metadataBase: new URL("https://www.aurigital.com/"),
   title: {
-    default: 'Aurigital - Sitios Web para Marcas que se Destacan | Desarrollo Web Costa Rica',
-    template: '%s | Aurigital'
+    default:
+      "Aurigital - Sitios Web para Marcas que se Destacan | Desarrollo Web Costa Rica",
+    template: "%s | Aurigital",
   },
-  description: 'Creamos sitios web estratégicos para marcas líderes en Costa Rica. Desarrollo web a medida, diseño profesional y experiencias digitales que impulsan tu crecimiento online.',
+  description:
+    "Creamos sitios web estratégicos para marcas líderes en Costa Rica. Desarrollo web a medida, diseño profesional y experiencias digitales que impulsan tu crecimiento online.",
   keywords: [
-    'desarrollo web Costa Rica',
-    'diseño web profesional',
-    'sitios web a medida',
-    'agencia digital Costa Rica',
-    'desarrollo web San José',
-    'páginas web empresariales',
-    'ecommerce Costa Rica',
-    'blog profesional',
-    'catálogo online',
-    'agenda web',
-    'landing pages',
-    'SEO Costa Rica',
-    'programación web',
-    'marcas digitales'
+    "desarrollo web Costa Rica",
+    "diseño web profesional",
+    "sitios web a medida",
+    "agencia digital Costa Rica",
+    "desarrollo web San José",
+    "páginas web empresariales",
+    "ecommerce Costa Rica",
+    "blog profesional",
+    "catálogo online",
+    "agenda web",
+    "landing pages",
+    "SEO Costa Rica",
+    "programación web",
+    "marcas digitales",
   ],
-  authors: [{ name: 'Aurigital Team' }],
-  creator: 'Aurigital',
-  publisher: 'Aurigital',
+  authors: [{ name: "Aurigital Team" }],
+  creator: "Aurigital",
+  publisher: "Aurigital",
   robots: {
     index: true,
     follow: true,
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   openGraph: {
-    type: 'website',
-    locale: 'es_CR',
-    url: 'https://www.aurigital.com',
-    siteName: 'Aurigital',
-    title: 'Aurigital - Sitios Web para Marcas que se Destacan',
-    description: 'Creamos páginas web estratégicas para marcas líderes en Costa Rica. Desarrollo web a medida, diseño web profesional y experiencias digitales que impulsan tu crecimiento online.',
+    type: "website",
+    locale: "es_CR",
+    url: "https://www.aurigital.com",
+    siteName: "Aurigital",
+    title: "Aurigital - Sitios Web para Marcas que se Destacan",
+    description:
+      "Creamos páginas web estratégicas para marcas líderes en Costa Rica. Desarrollo web a medida, diseño web profesional y experiencias digitales que impulsan tu crecimiento online.",
   },
   alternates: {
-    canonical: 'https://www.aurigital.com',
+    canonical: "https://www.aurigital.com",
     languages: {
-      'es-CR': 'https://www.aurigital.com',
-      'en-US': 'https://www.aurigital.com/en',
+      "es-CR": "https://www.aurigital.com",
+      "en-US": "https://www.aurigital.com/en",
     },
-  }
+  },
 };
 
 const schemaData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  "name": "Aurigital",
-  "description": "Agencia de desarrollo web especializada en sitios web estratégicos para marcas líderes en Costa Rica",
-  "url": "https://www.aurigital.com",
-  "logo": "https://www.aurigital.com/assets/logo.svg",
-  "email": "hola@aurigital.com",
-  "address": {
+  name: "Aurigital",
+  description:
+    "Agencia de desarrollo web especializada en sitios web estratégicos para marcas líderes en Costa Rica",
+  url: "https://www.aurigital.com",
+  logo: "https://www.aurigital.com/assets/logo.svg",
+  email: "hola@aurigital.com",
+  address: {
     "@type": "PostalAddress",
-    "streetAddress": "San José",
-    "addressLocality": "San José",
-    "addressRegion": "San José",
-    "postalCode": "10101",
-    "addressCountry": "CR"
+    streetAddress: "San José",
+    addressLocality: "San José",
+    addressRegion: "San José",
+    postalCode: "10101",
+    addressCountry: "CR",
   },
-  "foundingDate": "2023",
-  "numberOfEmployees": "5-10",
-  "serviceArea": {
+  foundingDate: "2023",
+  numberOfEmployees: "5-10",
+  serviceArea: {
     "@type": "Country",
-    "name": "Costa Rica"
+    name: "Costa Rica",
   },
-  "areaServed": "Costa Rica",
-  "hasOfferCatalog": {
+  areaServed: "Costa Rica",
+  hasOfferCatalog: {
     "@type": "OfferCatalog",
-    "name": "Servicios de Desarrollo Web",
-    "itemListElement": [
+    name: "Servicios de Desarrollo Web",
+    itemListElement: [
       {
         "@type": "Offer",
-        "itemOffered": {
+        itemOffered: {
           "@type": "Service",
-          "name": "Desarrollo Web",
-          "description": "Desarrollo de sitios web a medida"
-        }
+          name: "Desarrollo Web",
+          description: "Desarrollo de sitios web a medida",
+        },
       },
       {
         "@type": "Offer",
-        "itemOffered": {
+        itemOffered: {
           "@type": "Service",
-          "name": "Diseño Web",
-          "description": "Diseño web profesional y estratégico"
-        }
+          name: "Diseño Web",
+          description: "Diseño web profesional y estratégico",
+        },
       },
       {
         "@type": "Offer",
-        "itemOffered": {
+        itemOffered: {
           "@type": "Service",
-          "name": "Ecommerce",
-          "description": "Tiendas online intuitivas y seguras"
-        }
+          name: "Ecommerce",
+          description: "Tiendas online intuitivas y seguras",
+        },
       },
       {
         "@type": "Offer",
-        "itemOffered": {
+        itemOffered: {
           "@type": "Service",
-          "name": "Blog Profesional",
-          "description": "Blogs integrados para posicionamiento"
-        }
-      }
-    ]
+          name: "Blog Profesional",
+          description: "Blogs integrados para posicionamiento",
+        },
+      },
+    ],
   },
-  "openingHoursSpecification": [
+  openingHoursSpecification: [
     {
       "@type": "OpeningHoursSpecification",
-      "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-      "opens": "08:00",
-      "closes": "17:00"
-    }
+      dayOfWeek: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      opens: "08:00",
+      closes: "17:00",
+    },
   ],
-  "sameAs": [
+  sameAs: [
     "https://www.instagram.com/aurigital",
     "https://www.facebook.com/aurigital",
-    "https://www.linkedin.com/company/aurigital"
+    "https://www.linkedin.com/company/aurigital",
   ],
-  "knowsAbout": [
+  knowsAbout: [
     "Desarrollo Web",
     "Diseño Web",
     "E-commerce",
@@ -143,36 +154,134 @@ const schemaData = {
     "Landing Pages",
     "Blogs Profesionales",
     "Catálogos Online",
-    "Sistemas de Agendamiento"
-  ]
+    "Sistemas de Agendamiento",
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
+        {/* Links and meta tags only - NO SCRIPTS */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Questrial&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-              .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-              n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-              (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-              ml('account', '1023137');
-            `
-          }}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Questrial&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
+        {/* ALL SCRIPTS MOVED TO BODY */}
+        
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1416522006230127');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1416522006230127&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* Google Ads */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17131483110"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17131483110');
+          `}
+        </Script>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F79B9ETYTY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F79B9ETYTY');
+          `}
+        </Script>
+
+        {/* MailerLite */}
+        <Script id="mailerlite" strategy="afterInteractive">
+          {`
+            (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+            .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+            n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+            (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+            ml('account', '1023137');
+          `}
+        </Script>
+
+        {/* MailerLite Forms */}
+        <Script
+          src="https://groot.mailerlite.com/js/w/webforms.min.js?v176e10baa5e7ed80d35ae235be3d5024"
+          strategy="afterInteractive"
+        />
+
+        <Script id="mailerlite-form-load" strategy="afterInteractive">
+          {`
+            if (typeof ml !== 'undefined') {
+              fetch("https://assets.mailerlite.com/jsonp/1023137/forms/156290847285970148/takel");
+            }
+          `}
+        </Script>
+
+        {/* Schema.org Structured Data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+        />
+
+        {/* React Components */}
         <LanguageProvider>
           <ContactModalProvider>
             <LoadingProvider>
@@ -184,32 +293,6 @@ export default function RootLayout({ children }) {
             </LoadingProvider>
           </ContactModalProvider>
         </LanguageProvider>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
-        />
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-F79B9ETYTY" strategy="afterInteractive" />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-F79B9ETYTY');
-          `}
-        </Script>
-        
-        <Script 
-          src="https://groot.mailerlite.com/js/w/webforms.min.js?v176e10baa5e7ed80d35ae235be3d5024" 
-          strategy="afterInteractive"
-        />
-        
-        <Script id="mailerlite-form-load" strategy="afterInteractive">
-          {`
-            if (typeof ml !== 'undefined') {
-              fetch("https://assets.mailerlite.com/jsonp/1023137/forms/156290847285970148/takel");
-            }
-          `}
-        </Script>
       </body>
     </html>
   );
